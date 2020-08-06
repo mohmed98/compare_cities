66e5c1e4a105231f5360d41355985253674e4d84
unknown <mohaemdatta447@gmail.com>
2020-08-06 14:00:06 +0200

    all things are ready for start displayng data

---
+++ README.md
@@ -0,0 +1,1 @@
+comparing two cities weather project
--- index.js
+++ index.js
@@ -1,44 +1,86 @@
+// global vairables
+
 const wKey = "3684574286a446a6ac4122632200508";
 const submitBtn = document.getElementById("submitBtn");
-
+const resultArea = document.getElementById("resultSec");
+// the mail function that start the script when user submit cities names
 submitBtn.addEventListener("click", function () {
-
+  // getting user data
   const firstCityName = document.getElementById("firstCityName");
   const secondCityName = document.getElementById("secondCityName");
+  // inilize arrays for strong city weather daya for 8 days
+  let fistCitydata = [];
+  let secondCitydata = [];
+  // get request date and calulte 8 days bevore and store it in dates array
+  const dates = req_dates();
+  //fetch each day wether data
+  for (let i = 0; i < dates.length; i++) {
+    // first city fetch function call
+    getData(firstCityName.value, dates[i]).then((data) => {
+      fistCitydata.push(
+        // store response data in new object and then pushed it in array
+        new HisCityDayWeather(
+          data.location.name,
+          data.forecast.forecastday[0].day,
+          data.forecast.forecastday[0].date
+        )
+      );
 
-  console.log(firstCityName.value);
-  console.log(secondCityName.value);
-  const dates = req_dates()
-  for(let i = 2;i<dates.length;i++){
-    console.log(dates[i]);
-  fetch(
-    `http://api.weatherapi.com/v1/history.json?key=${wKey}&q=${firstCityName.value}&dt=${dates[2]}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
+      console.log(fistCitydata[i]);
+    });
 
-    .catch((err) => alert("Wrong city name!"));
-  fetch(
-    `http://api.weatherapi.com/v1/history.json?key=${wKey}&q=${secondCityName.value}&dt=${dates[i]}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
+    getData(secondCityName.value, dates[i]).then((data) => {
+      secondCitydata.push(
+        new HisCityDayWeather(
+          data.location.name,
+          data.forecast.forecastday[0].day,
+          data.forecast.forecastday[0].date
+        )
+      );
 
-    .catch((err) => alert("Wrong city name!"));
+      console.log(secondCitydata[i]);
+    });
   }
-
 });
-function req_dates(){
+// date function
+function req_dates() {
+  // request day date
   const today = new Date();
   let dateArrya = [];
-  for(let day=0;day<=7;day++){
-    const currentDay = new Date(today.getFullYear(), today.getMonth(), today.getDate()-day);
-    const dateFormat =`${currentDay.getFullYear()}-${currentDay.getMonth()+1}-${currentDay.getDate()}`;
+  // calulate 8 days perv the current one
+  for (let day = 0; day <= 7; day++) {
+    const currentDay = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() - day
+    );
+    // formate date to match api request format
+    const dateFormat = `${currentDay.getFullYear()}-${
+      currentDay.getMonth() + 1
+    }-${currentDay.getDate()}`;
     dateArrya.push(dateFormat);
   }
   return dateArrya;
+}
+// fetch function
+async function getData(cityName, date) {
+  try {
+    let response = await fetch(
+      `http://api.weatherapi.com/v1/history.json?key=${wKey}&q=${cityName}&dt=${date}`
+    );
+    let data = await response.json();
+    if (response.ok) {
+      return data;
+    } else {
+      return data["error"]["code"];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function HisCityDayWeather(name, dayweather, date) {
+  this.name = name;
+  this.day = dayweather;
+  this.date = date;
 }
